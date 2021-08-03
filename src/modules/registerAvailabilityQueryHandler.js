13b8bb4f9aa05ce479856bb247de0e89975cee5f
Sebastian Will <vttassets@gmail.com>
2021-08-03 13:58:16 +0200

    v2.0.2

--- src/modules/registerAvailabilityQueryHandler.js
+++ src/modules/registerAvailabilityQueryHandler.js
@@ -23,38 +23,38 @@ export default function () {
   });
 
   // register the default handler for all extension messages
-  window.addEventListener(config.messaging.extension.default, async (event) => {
-    const message = event.detail.data;
+  // window.addEventListener(config.messaging.extension.default, async (event) => {
+  //   const message = event.detail.data;
 
-    switch (message.type) {
-      case "CONNECT":
-        if (
-          message.data &&
-          message.data.user &&
-          message.data.user.permissions &&
-          Array.isArray(message.data.user.permissions) &&
-          message.data.user.permissions.includes("reportBugs") === true
-          //message.data.user.permissions.includes("none") === true
-        ) {
-          game.settings.set(
-            config.module.name,
-            "access_token",
-            message.data.user.token
-          );
-          /**
-           * Registering the bug report app
-           */
-          if (game.settings.menus.get("vtta-core.bug-report") === undefined) {
-            game.settings.registerMenu(config.module.name, "bug-report", {
-              type: BugReport,
-              name: "BUGREPORT.vtta-core.name",
-              label: "BUGREPORT.vtta-core.label",
-              hint: "BUGREPORT.vtta-core.hint",
-              restricted: true,
-            });
-          }
-        }
-        break;
-    }
-  });
+  //   switch (message.type) {
+  //     case "CONNECT":
+  //       if (
+  //         message.data &&
+  //         message.data.user &&
+  //         message.data.user.permissions &&
+  //         Array.isArray(message.data.user.permissions) &&
+  //         message.data.user.permissions.includes("reportBugs") === true
+  //         //message.data.user.permissions.includes("none") === true
+  //       ) {
+  //         game.settings.set(
+  //           config.module.name,
+  //           "access_token",
+  //           message.data.user.token
+  //         );
+  //         /**
+  //          * Registering the bug report app
+  //          */
+  //         if (game.settings.menus.get("vtta-core.bug-report") === undefined) {
+  //           game.settings.registerMenu(config.module.name, "bug-report", {
+  //             type: BugReport,
+  //             name: "BUGREPORT.vtta-core.name",
+  //             label: "BUGREPORT.vtta-core.label",
+  //             hint: "BUGREPORT.vtta-core.hint",
+  //             restricted: true,
+  //           });
+  //         }
+  //       }
+  //       break;
+  //   }
+  // });
 }
